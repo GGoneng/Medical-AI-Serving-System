1a2e859609c87cf1c5b41b91634bfe67ed9a3bf4
GGoneng <zxzx0031@naver.com>
2026-01-17 15:41:47 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   README.md
    	deleted:    zz.py

--- zz.py
+++
@@ -1,6 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# D드라이브 내 원하는 폴더에 모델 저장
-model_path = snapshot_download("snuh/hari-q3", cache_dir="F:/huggingface_models")
-
-print("모델 다운로드 완료 경로:", model_path)
